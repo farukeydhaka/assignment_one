d0525e1fa557c703efce5858435a0656f6f92390
Faruk <faruk.eydhaka@gmail.com>
2024-07-06 16:34:17 +0600

    First submited

---
+++ laggest_number.php
@@ -0,0 +1,18 @@
+
+
+<?php
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if ($num1 > $num2 && $num1 > $num3 ) {
+  	echo "Largest Number is  $num1";
+		}elseif ($num2>$num1 && $num2>$num3) {
+			echo "Largest Number is $num2";
+    	}elseif ($num3>$num1 && $num3>$num2){
+    		echo "Largest Number is $num3";}
+?>
+ 
+
+    
+
